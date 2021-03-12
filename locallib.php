a830148b82e58124dede173b6613cf65674003d6
Vitaliy Shapovalov <xflash55@yandex.ru>
2021-03-12 15:30:16 +0300

    add func assignment session screen by default

--- locallib.php
+++ locallib.php
@@ -10,3 +10,21 @@ defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG -> dirroot . '/lib/uploadlib.php');
 
+function helpdesk_resolve_screen(){
+    global $SESSION;
+
+    $context = context_system::instance();
+
+    $screen = optional_param('screen', @$SESSION->tracker_current_screen, PARAM_ALPHA);
+
+    if(empty($screen)){
+        if(has_capability('local/helpdesk:develop', $context)){
+            $defaultscreen = 'mywork';
+        } elseif (has_capability('local/helpdesk:report', $context)) {
+            $defaultscreen = 'mytickets';
+        } else {
+            $defaultscreen = 'browse';
+        }
+        $screen = $defaultscreen;
+    }
+}
