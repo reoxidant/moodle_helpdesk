d805b7029c16ab32468ebc29789f56639eefee49
Vitaliy Shapovalov <xflash55@yandex.ru>
2021-04-21 17:22:18 +0300

    feat(db): add access

--- db/access.php
+++ db/access.php
@@ -5,3 +5,37 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @package PhpStorm
  */
+
+
+$capabilities = array(
+    'local/helpdesk:view_all_issues' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetype' => array(
+            'manager' => CAP_ALLOW
+        )
+    ),
+
+    'local/helpdesk:report' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetype' => array(
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        )
+    ),
+
+    'local/helpdesk:develop' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetype' => array(
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        )
+    ),
+
+);
