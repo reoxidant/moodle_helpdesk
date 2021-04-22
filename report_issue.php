a11bd41f93ecdeca6a303f3c87ccd44323db6313
Vitaliy Shapovalov <xflash55@yandex.ru>
2021-04-22 15:36:58 +0300

    refactor(project): rename files using correct name

---
+++ report_issue.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Description actions
+ * @copyright 2021 vshapovalov
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package PhpStorm
+ */
+
+require('../../config.php');
+require_once($CFG->dirroot. '/local/helpdesk/lib.php');
+require_once($CFG->dirroot. '/local/helpdesk/locallib.php');
+require_once($CFG->dirroot. '/local/helpdesk/forms/reportissue_form.php');
