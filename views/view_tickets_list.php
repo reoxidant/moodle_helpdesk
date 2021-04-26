7cadfea11d78207106fc78cf0ae2bc929058f885
Vitaliy Shapovalov <xflash55@yandex.ru>
2021-04-26 17:13:51 +0300

    feat(view tickets): create table for show all records

--- views/view_tickets_list.php
+++ views/view_tickets_list.php
@@ -5,3 +5,9 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @package PhpStorm
  */
+
+if (!defined('MOODLE_INTERNAL')) {
+    die('Direct access to this script is forbidden');
+}
+
+require_once($CFG -> libdir . '/tablelib.php');
