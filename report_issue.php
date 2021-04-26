74e539eee85bb0df223543fe5d5940f6a0b48fb5
Vitaliy Shapovalov <xflash55@yandex.ru>
2021-04-26 17:12:00 +0300

    refactor(report_issue): fix space lines

--- report_issue.php
+++ report_issue.php
@@ -7,14 +7,14 @@
  */
 
 require('../../config.php');
-require_once($CFG->dirroot. '/local/helpdesk/lib.php');
-require_once($CFG->dirroot. '/local/helpdesk/locallib.php');
-require_once($CFG->dirroot. '/local/helpdesk/forms/report_issue_form.php');
+require_once($CFG -> dirroot . '/local/helpdesk/lib.php');
+require_once($CFG -> dirroot . '/local/helpdesk/locallib.php');
+require_once($CFG -> dirroot . '/local/helpdesk/forms/report_issue_form.php');
 
 $screen = helpdesk_resolve_screen();
 $view = helpdesk_resolve_view();
 
-$context = context_system::instance();
+$context = context_system ::instance();
 
 require_login();
 require_capability('local/helpdesk:report', $context);
@@ -34,39 +34,39 @@ $PAGE -> set_url($url);
 $form = new HelpDeskIssueForm($url);
 
 if (!$form -> is_cancelled() && $data = $form -> get_data()) {
-    if (!$issue = helpdesk_submit_issue_form($data)){
-        print_error('error_can_not_submit_ticket', 'local_helpdesk');
+    if (!$issue = helpdesk_submit_issue_form($data)) {
+        print_error('error_cannot_submit_ticket', 'local_helpdesk');
     }
 
     $data = file_postupdate_standard_editor(
         $data,
         'description',
-        $form->options,
+        $form -> options,
         $context,
         'local_helpdesk',
         'issuedescription',
-        $data->issueid
+        $data -> issueid
     );
 
     $stc = new StdClass;
-    $stc->userid = $USER->id;
-    $stc->issueid = $issue->id;
-    $stc->timechange = time();
-    $stc->statusfrom = POSTED;
-    $stc->statusto = POSTED;
-    echo $OUTPUT->header();
-    echo $OUTPUT->box_start('generalbox', 'helpdesk-acknowledge');
+    $stc -> userid = $USER -> id;
+    $stc -> issueid = $issue -> id;
+    $stc -> timechange = time();
+    $stc -> statusfrom = POSTED;
+    $stc -> statusto = POSTED;
+    echo $OUTPUT -> header();
+    echo $OUTPUT -> box_start('generalbox', 'helpdesk-acknowledge');
     echo get_string('thanks_default', 'local_helpdesk');
-    echo $OUTPUT->box_end();
-    echo $OUTPUT->continue_button(new moodle_url('/local/helpdesk/view.php', array('view' => 'view', 'screen' => 'browse')));
-    echo $OUTPUT->footer();
+    echo $OUTPUT -> box_end();
+    echo $OUTPUT -> continue_button(new moodle_url('/local/helpdesk/view.php', array('view' => 'view', 'screen' => 'browse')));
+    echo $OUTPUT -> footer();
 }
 
-echo $OUTPUT->header();
+echo $OUTPUT -> header();
 
 $view = 'report_issue';
-include_once($CFG->dirroot.'/local/helpdesk/menus.php');
+include_once($CFG -> dirroot . '/local/helpdesk/menus.php');
 
-$form->display();
+$form -> display();
 
-echo $OUTPUT->footer();
+echo $OUTPUT -> footer();
