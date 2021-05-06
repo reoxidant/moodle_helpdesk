3bfbcc616d5e2457ca2f187eeac03a80f2f9af05
reoxidant <kupper133@gmail.com>
2021-05-06 21:37:01 +0300

    update(version): new version.php

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin -> component = 'local_helpdesk';   // Declare the type and name of this plugin.
-$plugin -> version = 2021050401;  // Plugin released on 20th Mar 2021.
-$plugin -> requires = 2019052000; // Moodle 3.7.0 is required.
-$plugin -> supported = [37, 39]; // Moodle 3.7.x, 3.8.x and 3.9.x are supported.
-$plugin -> maturity = MATURITY_STABLE; // This is considered as ready for production sites.
-$plugin -> release = '0.0.1 (Build 2021031000)'; // This is our first revision for Moodle 2.7.x branch.
+$plugin->component = 'local_helpdesk';   // Declare the type and name of this plugin.
+$plugin->version = 2021050600;  // Plugin released on 20th Mar 2021.
+$plugin->requires = 2019052000; // Moodle 3.7.0 is required.
+$plugin->supported = [37, 39]; // Moodle 3.7.x, 3.8.x and 3.9.x are supported.
+$plugin->maturity = MATURITY_STABLE; // This is considered as ready for production sites.
+$plugin->release = '0.0.1 (Build 2021031000)'; // This is our first revision for Moodle 2.7.x branch.
