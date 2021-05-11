549d97c1e733f56484d33e01a2c0911f449fb732
Vitaliy Shapovalov <xflash55@yandex.ru>
2021-05-11 17:24:29 +0300

    feat(viewcontroller): add handler for updating the status list issues

--- views/viewcontroller.php
+++ views/viewcontroller.php
@@ -2,26 +2,32 @@
 
 if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.');
-} elseif ($action === 'updateanissues') {
-    $issue = new StdClass;
+}
 
-    $issue -> id = required_param('issueid', PARAM_INT);
-    $issue -> issueid = $issue -> id;
-    $issue -> status = required_param('status', PARAM_INT);
-    $issue -> assignedto = required_param('assignedto', PARAM_INT);
-    $issue -> summary = required_param('summary', PARAM_TEXT);
-    $issue -> description_editor = required_param_array('description_editor', PARAM_CLEANHTML);
-    $issue -> description_format = $issue -> description_editor['format'];
-    $editoptions = array('maxfiles' => 99);
-
-    $issue -> resolution_editor = required_param_array('resolution_editor', PARAM_CLEANHTML);
-    $issue -> resolution_format = $issue -> resolution_editor['format'];
-
-    $issue -> description = file_save_draft_area_files($issue -> description_editor['itemid'], $context -> id, 'local_helpdesk', 'issuedescription', $issue -> id, $editoroptions, $issue -> description_editor['text']);
-    $issue -> resolution = file_save_draft_area_files($issue -> resolution_editor['itemid'], $context -> id, 'local_helpdesk', 'issueresolution', $issue -> id, $editoroptions, $issue -> resolution_editor['text']);
-
-    $issue -> datereported = required_param('datereported', PARAM_INT);
-
-    $issue -> bywhomid = $USER -> id;
-    $issue -> timeassigned = time();
+if ($action === 'updatelist') {
+    $keys = array_keys($_POST);
+    $statuskeys = preg_grep('/status./', $keys);
+    $assignedtokeys = preg_grep('/assignedto./', $keys);
+    $newassignedtokeys = preg_grep('/assignedtoi./', $keys);
+    foreach ($statuskeys as $akey) {
+        $issueid = str_replace('status', '', $akey);
+        $haschanged = optional_param('schanged' . $issueid, 0, PARAM_INT);
+        if ($haschanged) {
+            $issue = new StdClass;
+            $issue -> id = $issueid;
+            $issue -> status = required_param($akey, PARAM_INT);
+            $oldstatus = $DB -> get_field('helpdesk_issue', 'status', ['id' => $issue -> id]);
+            $DB -> update_record('helpdesk_issue', $issue);
+            // MARK: check status changing and send notifications
+            if ($oldstatus !== $issue -> status) {
+                $stc = new StdClass;
+                $stc -> usedid = $USER -> id;
+                $stc -> issueid = $issue -> id;
+                $stc -> timechange = time();
+                $stc -> statusform = $oldstatus;
+                $stc -> statusto = $issue -> status;
+                $DB -> insert_record('', $stc);
+            }
+        }
+    }
 }
